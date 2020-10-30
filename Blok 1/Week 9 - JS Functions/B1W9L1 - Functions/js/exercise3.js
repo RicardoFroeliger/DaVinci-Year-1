f93a6fb18400e27710bce252cfe9d571aa0e046c
Ricardo Froeliger <99064304@mydavinci.nl>
2020-10-30 11:16:40 +0100

    Updated: Blok 1, Week 9

--- Blok 1/Week 9 - JS Functions/B1W9L1 - Functions/js/exercise3.js
+++ Blok 1/Week 9 - JS Functions/B1W9L1 - Functions/js/exercise3.js
@@ -9,18 +9,18 @@ let stop = false,
 
 /* Collector Function */
 let collect = type => {
-    if(type == 'name') {
-        colReq = num == 1 ? 'Please enter your name.' : `Please enter name ${num}.`;
+    if(type === 'name') {
+        colReq = num === 1 ? 'Please enter your name.' : `Please enter name ${num}.`;
         colErr = 'Incorrect format! You canceled or didn\'t enter anything.\nPlease refresh this page and re-enter name (s) and date of birth (s).';
-    } else if(type == 'bDate') {
-        colReq = num == 1 ? 'Please enter your birthdate.' : `Please enter birthdate ${num} (dd-mm-yyyy).`; num++;
+    } else if(type === 'bDate') {
+        colReq = num === 1 ? 'Please enter your birthdate.' : `Please enter birthdate ${num} (dd-mm-yyyy).`; num++;
         colErr = 'Incorrect format! You canceled or didn\'t enter a valid format.\nBirth dates must be in dd-mm-yyyy format.\nPlease refresh this page and re-enter name (s) and date of birth (s).'
     };
 
     let collected = prompt(colReq);
-    if(collected && collected.toLowerCase() == 'stop') { stop = true; noData(); }
+    if(collected && collected.toLowerCase() === 'stop') { stop = true; noData(); }
     // Basic filter, depending on what the type is the filter changes.
-    else if(type == 'name' ? collected : collected && collected.length >= 10) {
+    else if(type === 'name' ? collected : collected && collected.length >= 10) {
         data.push(collected); 
         console.log(data);
     } else { alert(colErr); stop = true; };
@@ -29,11 +29,11 @@ let collect = type => {
 /* Finalize / Execute */
 setTimeout(() => {
     if(confirm('Welcome!\nPlease enter the names of people and their date of birth.\nIf done, type \'stop\'.')) {
-        while(stop == false) {
+        while(stop === false) {
             collect('name');
-            if(stop == false) collect('bDate');
+            if(stop === false) collect('bDate');
         };
-        if(stop == true) {
+        if(stop === true) {
             let output = [`Hello ${data[0] ? data[0].capitalize() : null}, you entered your date of birth on ${data[1] ? data[1] : null}.<br>`];
 
             let ind = 2;
