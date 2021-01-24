1b4e3edd487f770516eda0a18facd0dd09bad867
Ricardo Froeliger <99064304@mydavinci.nl>
2021-01-24 21:00:23 +0100

    Updated: Blok 1, Week 9

--- Blok 1/Week 9 - JS Functions/B1W9L2 - Fibonacci Sequence/script.js
+++ Blok 1/Week 9 - JS Functions/B1W9L2 - Fibonacci Sequence/script.js
@@ -1,9 +1,7 @@
-let output = [], quantity = 20;
+let output = [], quantity = 20; // Max: 1477 
 
 for(let i = 0; i < quantity - 1; i++) {
     i == 0 ? output.push(0, 1) : output.push(output[i - 1] + output[i]);
 };
 
-setTimeout(() => {
-    document.getElementById('output').innerHTML = output.join(', ');
-}, 100);
+window.onload = () => document.getElementById('output').innerHTML = output.join(', ');
